72d45d11f1b5e1f0e6ee1d442cbf0878c8c0d899
Nathan Dotz <nathan.dotz@gmail.com>
2012-05-09 18:30:31 -0400

    Update so signed applet displays in browser

--- src/MicDataSpewer.java
+++ src/MicDataSpewer.java
@@ -1,7 +1,12 @@
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Scrollbar;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.ShortBuffer;
 
 import javax.sound.sampled.AudioFormat;
@@ -9,36 +14,40 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.TargetDataLine;
 
-public class MicDataSpewer extends Applet {
+public class MicDataSpewer extends Applet implements AdjustmentListener {
 	private static final long serialVersionUID = 1L;
 	TargetDataLine dataline;
 	SamplingThread sampleThread;
 	AudioFormat audioFormat;
+	int multiplier;
 
+	private short [] sbAsArrays;
+	
+	Scrollbar slider = new Scrollbar(Scrollbar.VERTICAL, 0 , 1, 0, 100);
+	
 	boolean errorState;
 	String errMsg;
-	Float avgAmp = 0f;
-	
-	Float runningAvg = 0f;
-	Integer runningAvgCount = 0;
-
 	public void start() {
 		audioFormat = new AudioFormat(44100, 16, 1, true, false);
 		DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class,
 				audioFormat);
 		System.out.println(dataLineInfo);
 		
+		add(slider);
+		slider.addAdjustmentListener(this);
+
 		try {
 			dataline = (TargetDataLine) AudioSystem.getLine(dataLineInfo);
 			System.out.println(dataline);
 			dataline.open(audioFormat);
-		} catch (Exception e){
+		} catch (Exception e) {
 			errorState = true;
-			errMsg = "Could not open data line for AudioSystem -- " + e.getMessage();
+			errMsg = "Could not open data line for AudioSystem -- "
+					+ e.getMessage();
 			repaint();
 			return;
 		}
-		
+
 		try {
 			sampleThread = new SamplingThread();
 			sampleThread.start();
@@ -51,34 +60,33 @@ public class MicDataSpewer extends Applet {
 
 	public class SamplingThread extends Thread {
 		boolean running = false;
-
 		public void run() {
 			running = true;
 			try {
-				int sampleSize = (int) (audioFormat.getSampleRate() * audioFormat.getFrameSize());
+				int sampleSize = (int) (audioFormat.getFrameSize()
+						* audioFormat.getChannels()
+						* audioFormat.getSampleRate()) / 50; // 20ms
+				System.out.println("Sample Size is: " + sampleSize + " bytes");
+				System.out.println("Byte-order is big edian?: "+audioFormat.isBigEndian());
 				byte sampleBuffer[] = new byte[sampleSize];
 				dataline.start();
 
 				while (running
 						&& dataline.read(sampleBuffer, 0, sampleSize) > 0) {
-					avgAmp = 0f;
 					ByteBuffer bob = ByteBuffer.wrap(sampleBuffer);
-					ShortBuffer sb = bob.asShortBuffer();
-					System.out.println(bob.order().toString() + ' ' + sb.order().toString());
-					
-					while (sb.hasRemaining()) {
-						avgAmp += sb.get();
+					if (audioFormat.isBigEndian()) {
+						bob.order(ByteOrder.BIG_ENDIAN);						
+					} else {
+						bob.order(ByteOrder.LITTLE_ENDIAN);
 					}
-					avgAmp /= sb.capacity();
+					ShortBuffer sb = bob.asShortBuffer();
+					sbAsArrays = new short[sb.capacity()];
+					sb.get(sbAsArrays);
 
-					//runningAvg = (avgAmp + runningAvgCount*runningAvg)/(runningAvgCount+1);
-					runningAvg = avgAmp;
-					runningAvgCount++;
-					
 					repaint();
 
 				}
-				
+
 				dataline.flush();
 				dataline.close();
 
@@ -91,6 +99,11 @@ public class MicDataSpewer extends Applet {
 
 	}
 
+	int map(int x, int in_min, int in_max, int out_min, int out_max)
+	{
+	  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	}
+	
 	public void paint(Graphics g) {
 
 		if (errorState) {
@@ -99,12 +112,31 @@ public class MicDataSpewer extends Applet {
 			this.sampleThread.running = false;
 			return;
 		}
-		if (avgAmp != 0) {
-			g.drawString("Average: " + runningAvg.toString(), 20, 60);
-			g.drawString("Count: " + runningAvgCount.toString(), 20, 80);
+		if (sbAsArrays != null) {
+//			g.drawString("Average: " + runningAvg.toString(), 20, 60);
+//			g.drawString("Count: " + runningAvgCount.toString(), 20, 80);
+			
+			Rectangle drawingSpace = g.getClipBounds();
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, drawingSpace.width, drawingSpace.height);
+			for (int i = 1; i<sbAsArrays.length; i++) {
+				g.setColor(Color.GREEN);
+				int oldX = map(i-1, 0, sbAsArrays.length, 0, drawingSpace.width);
+				int newX = map(i, 0, sbAsArrays.length, 0, drawingSpace.width);
+				int oldY = map(sbAsArrays[i-1]*multiplier, Short.MAX_VALUE, Short.MIN_VALUE, 0, drawingSpace.height);
+				int newY = map(sbAsArrays[i]*multiplier, Short.MAX_VALUE, Short.MIN_VALUE, 0, drawingSpace.height);
+
+				g.drawLine(oldX, oldY, newX, newY);
+			}
 		} else {
 			g.drawString("Loading...", 20, 60);
 		}
+	}
+
+	@Override
+	public void adjustmentValueChanged(AdjustmentEvent arg0) {
+		multiplier = slider.getValue() * 50 / 100;
+		repaint();
 	}
 
 }
